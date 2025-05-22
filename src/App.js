625cb42160886020f2f5179038a0af32f86cd580
henokmulat <henokmulat27@gmail.com>
2025-05-22 17:08:43 +0300

    implement component lifecycle methods

--- src/App.js
+++ src/App.js
@@ -34,13 +34,14 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: "lisbon",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
   };
 
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
     // async fetchWeather() {
     try {
       this.setState({ isLoading: true });
@@ -66,13 +67,25 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+  // useEffect []
+  componentDidMount() {
+    // this.fetchWeather();
+    this.setState({ location: localStorage.getItem("location") || "" });
+  }
+  // useEffect [location]
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+      localStorage.setItem("location", this.state.location);
+    }
+  }
   render() {
     return (
       <div className="app">
@@ -81,7 +94,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}> Get weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -114,6 +126,9 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log("component is unmount");
+  }
   render() {
     const {
       temperature_2m_max: max,
